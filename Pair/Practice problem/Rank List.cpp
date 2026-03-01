9bd48354aa431dd5378a0f40e0d570849850439f
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-03-01 23:22:20 +0600

    Add files via upload

---
+++ Pair/Practice problem/Rank List.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b)
+{
+    if(a.first>b.first) return 1;
+    else if(a.first==b.first) return (a.second<b.second);
+    else return 0;  
+}
+
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<pair<int,int>> v(n);
+
+    for(int i=0;i<n;i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    sort(v.begin(),v.end(),cmp);
+
+    int ans = 0;
+
+    for(auto u : v)
+    {
+        if(u == v[k-1])
+        ans++;
+
+    }
+    cout<< ans;
+}
